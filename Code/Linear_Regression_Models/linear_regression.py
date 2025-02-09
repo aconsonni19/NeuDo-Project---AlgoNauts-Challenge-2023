d3e6189ae24cb639ff73df725c15608718b55d2e
spitfire233 <87020972+spitfire233@users.noreply.github.com>
2025-02-09 03:53:23 +0100

    Project restructure; added and finished training script

---
+++ Code/Linear_Regression_Models/linear_regression.py
@@ -0,0 +1,36 @@
+# ================================================================================================
+# Python module to call the various linear regression models contained in Linear_Regression_Models
+# ================================================================================================
+import os.path
+
+from Code.Linear_Regression_Models.baseline_encoding_model import \
+    baseline_encoding_extract_features, baseline_encoding_train_encoding_model
+
+### Local dirs
+project_dir = "../../"
+output_dir = "../../Outputs"
+linear_regression_models_dir = os.path.join(output_dir, 'Linear_regression_models')
+
+
+def use_algonauts_model(project_dir, output_dir):
+    """""
+    Utilizes the model contain in algonauts2023_baseline_model
+    :param sub:
+        Subject used
+    :param project_dir:
+        project directory
+    :param output_dir:
+        output directory
+    """
+    local_baseline_encoding_dir = os.path.join(output_dir, 'baseline_encoding_model')
+    if os.path.isdir(local_baseline_encoding_dir) == False:
+        os.makedirs(local_baseline_encoding_dir)
+
+    for sub in range (1, 9):
+       #baseline_encoding_extract_features(sub, project_dir, local_baseline_encoding_dir) # Extract features
+        baseline_encoding_train_encoding_model(sub, project_dir, output_dir) # Train model
+
+
+
+use_algonauts_model(project_dir, linear_regression_models_dir)
+
